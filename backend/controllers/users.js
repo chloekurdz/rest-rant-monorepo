702224c4bf0bd38ab2afed73854a657fe3069e27
Chloe Kurdziel <chloekurdziel@gmail.com>
2023-02-08 13:05:54 -0500

    final changes for assignment

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -4,8 +4,10 @@ const db = require("../models")
 const { User } = db
 
 router.post('/', async (req, res) => {
+    let { password, ...rest } = req.body;
     const user = await User.create({
-        ...resizeTo,
+        ...rest,
+        role: 'reviewer',
         passwordDigest: await bcrypt.hash(password, 10)
     })
     res.json(user)
